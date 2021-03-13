37397d00a991fe0a496b9e2c3b2578255e74a203
Sridharan <shrisridharan@yahoo.in>
2021-03-13 23:24:10 +0000

    Removed body parser as express 4.17 has bodyparser included by default

---
+++ routes/admin.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use('/add-products', (req, res, next) => {
+    
+});
+
+router.post('/products', (req, res, next) => {
+
+});
